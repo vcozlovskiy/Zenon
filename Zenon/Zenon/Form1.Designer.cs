9e11d86b1a285a4bc90af39b9b7d72d53fb33e81
vcozlovskiy <38006998+vcozlovskiy@users.noreply.github.com>
2020-04-12 11:05:50 +0300

    Add files via upload

---
+++ Zenon/Zenon/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace Zenon
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.vax = new System.Windows.Forms.TextBox();
+            this.vtor = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.start = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Info;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 135);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(703, 478);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.PictureBox1_Paint);
+            // 
+            // vax
+            // 
+            this.vax.Location = new System.Drawing.Point(151, 27);
+            this.vax.Name = "vax";
+            this.vax.Size = new System.Drawing.Size(100, 22);
+            this.vax.TabIndex = 1;
+            // 
+            // vtor
+            // 
+            this.vtor.Location = new System.Drawing.Point(151, 52);
+            this.vtor.Name = "vtor";
+            this.vtor.Size = new System.Drawing.Size(100, 22);
+            this.vtor.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "VELOSITI AXILeS";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(132, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "VELOSITI TORTILA";
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(285, 27);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(75, 23);
+            this.start.TabIndex = 5;
+            this.start.Text = "Поехали";
+            this.start.UseVisualStyleBackColor = true;
+           
+            this.start.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Start_MouseClick);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(285, 55);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Приехали";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Button1_MouseClick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(366, 57);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Not reali";
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.Timer2_Tick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(448, 24);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "STOP";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Button2_MouseClick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(448, 57);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(727, 625);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.vtor);
+            this.Controls.Add(this.vax);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox vax;
+        private System.Windows.Forms.TextBox vtor;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Zenon/Zenon/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Zenon
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        float t = 0, dt, x = 0, y = 200, vaxil = 0, vtort = 0, d = 0;
+        public static int AWidth { get; } = 40;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+        
+        private void Start_MouseClick(object sender, MouseEventArgs e)
+        {
+            timer1.Start();
+            timer1.Interval = 10;
+            float.TryParse(vax.Text, out vaxil);
+            float.TryParse(vtor.Text, out vtort); 
+        }
+        private void PictureBox1_Paint(object sender, PaintEventArgs e)
+        {   
+
+            Graphics grf = e.Graphics;
+            Pen pen = new Pen(Color.Red, 3);
+            Pen pen2 = new Pen(Color.Green, 3);
+            grf.DrawRectangle(pen,x, 285, 100, 100);
+            grf.DrawEllipse(pen2,y, 360, 50, 50);
+        }
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            if (x>478)
+            {
+                x = -100;
+            }
+            if (y > 528)
+            {
+                y = -50;
+            }
+            x += (int)vaxil;
+            y += (int)vtort;
+            pictureBox1.Refresh();
+        }
+        private void Timer2_Tick(object sender, EventArgs e)
+        {
+            if (x > 478)
+            {
+                x = -100;
+            }
+            if (y > 528)
+            {
+                y = -50;
+            }
+            d = y - x ;
+            t = d / vaxil;
+            dt = t / 100;
+            x += vaxil * dt;
+            y += vtort * dt;
+            pictureBox1.Refresh();
+        }
+        private void Button1_MouseClick(object sender, MouseEventArgs e)
+        {
+            timer2.Start();
+            timer2.Interval = 100;
+            float.TryParse(vax.Text, out vaxil);
+            float.TryParse(vtor.Text, out vtort);
+        }
+
+        private void Button2_MouseClick(object sender, MouseEventArgs e)
+        {
+            x = 0;
+            y = 200;
+            timer1.Stop();
+            timer2.Stop();
+        }
+    }
+}
